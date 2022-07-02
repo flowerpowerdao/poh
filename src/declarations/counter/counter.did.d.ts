435a865f18b5634c802af9d95af2c553cfbb8d0c
Moritz <fullermoritz@gmail.com>
2022-07-02 12:46:42 +0200

    🚚 renamed canister

--- src/declarations/counter/counter.did.d.ts
+++ src/declarations/counter/counter.did.d.ts
@@ -1,5 +1,58 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
-export interface Whitelist { 'setup' : ActorMethod<[], string> }
+export interface ChallengeResponse {
+  'status' : PohChallengeStatus,
+  'completedOn' : [] | [bigint],
+  'challengeId' : string,
+}
+export type CheckStatusError = { 'pohAlreadyInitiated' : null } |
+  { 'principalBlacklisted' : null } |
+  { 'alreadyWhitelisted' : null } |
+  { 'noTokenFound' : null } |
+  { 'notFirstAssociation' : null } |
+  { 'pohNotCompleted' : null } |
+  { 'pohRejected' : null };
+export type PohChallengeStatus = { 'notSubmitted' : null } |
+  { 'verified' : null } |
+  { 'expired' : null } |
+  { 'pending' : null } |
+  { 'rejected' : null };
+export interface PohVerificationResponsePlus {
+  'status' : PohVerificationStatus,
+  'completedAt' : [] | [bigint],
+  'token' : [] | [string],
+  'rejectionReasons' : Array<string>,
+  'submittedAt' : [] | [bigint],
+  'isFirstAssociation' : boolean,
+  'providerId' : Principal,
+  'challenges' : Array<ChallengeResponse>,
+  'requestedAt' : [] | [bigint],
+  'providerUserId' : string,
+}
+export type PohVerificationStatus = { 'notSubmitted' : null } |
+  { 'verified' : null } |
+  { 'expired' : null } |
+  { 'pending' : null } |
+  { 'startPoh' : null } |
+  { 'rejected' : null };
+export type Result = { 'ok' : null } |
+  { 'err' : CheckStatusError };
+export interface Whitelist {
+  'callback' : ActorMethod<[PohVerificationResponsePlus], undefined>,
+  'checkStatus' : ActorMethod<[], Result>,
+  'getBlacklist' : ActorMethod<[], Array<Principal>>,
+  'getBlacklistQuery' : ActorMethod<[], Array<Principal>>,
+  'getQueue' : ActorMethod<[], Array<[Principal, string]>>,
+  'getQueueQuery' : ActorMethod<[], Array<[Principal, string]>>,
+  'getWhitelist' : ActorMethod<[], Array<Principal>>,
+  'getWhitelistQuery' : ActorMethod<[], Array<Principal>>,
+  'isBlacklisted' : ActorMethod<[Principal], boolean>,
+  'isBlacklistedQuery' : ActorMethod<[Principal], boolean>,
+  'isQueued' : ActorMethod<[Principal], boolean>,
+  'isQueuedQuery' : ActorMethod<[Principal], boolean>,
+  'isWhitelisted' : ActorMethod<[Principal], boolean>,
+  'isWhitelistedQuery' : ActorMethod<[Principal], boolean>,
+  'registerCallback' : ActorMethod<[], undefined>,
+}
 export interface _SERVICE extends Whitelist {}
--- src/declarations/counter/counter.did.js
+++ src/declarations/counter/counter.did.js
@@ -1,5 +1,71 @@
 export const idlFactory = ({ IDL }) => {
-  const Whitelist = IDL.Service({ 'setup' : IDL.Func([], [IDL.Text], []) });
+  const PohVerificationStatus = IDL.Variant({
+    'notSubmitted' : IDL.Null,
+    'verified' : IDL.Null,
+    'expired' : IDL.Null,
+    'pending' : IDL.Null,
+    'startPoh' : IDL.Null,
+    'rejected' : IDL.Null,
+  });
+  const PohChallengeStatus = IDL.Variant({
+    'notSubmitted' : IDL.Null,
+    'verified' : IDL.Null,
+    'expired' : IDL.Null,
+    'pending' : IDL.Null,
+    'rejected' : IDL.Null,
+  });
+  const ChallengeResponse = IDL.Record({
+    'status' : PohChallengeStatus,
+    'completedOn' : IDL.Opt(IDL.Int),
+    'challengeId' : IDL.Text,
+  });
+  const PohVerificationResponsePlus = IDL.Record({
+    'status' : PohVerificationStatus,
+    'completedAt' : IDL.Opt(IDL.Int),
+    'token' : IDL.Opt(IDL.Text),
+    'rejectionReasons' : IDL.Vec(IDL.Text),
+    'submittedAt' : IDL.Opt(IDL.Int),
+    'isFirstAssociation' : IDL.Bool,
+    'providerId' : IDL.Principal,
+    'challenges' : IDL.Vec(ChallengeResponse),
+    'requestedAt' : IDL.Opt(IDL.Int),
+    'providerUserId' : IDL.Text,
+  });
+  const CheckStatusError = IDL.Variant({
+    'pohAlreadyInitiated' : IDL.Null,
+    'principalBlacklisted' : IDL.Null,
+    'alreadyWhitelisted' : IDL.Null,
+    'noTokenFound' : IDL.Null,
+    'notFirstAssociation' : IDL.Null,
+    'pohNotCompleted' : IDL.Null,
+    'pohRejected' : IDL.Null,
+  });
+  const Result = IDL.Variant({ 'ok' : IDL.Null, 'err' : CheckStatusError });
+  const Whitelist = IDL.Service({
+    'callback' : IDL.Func([PohVerificationResponsePlus], [], ['oneway']),
+    'checkStatus' : IDL.Func([], [Result], []),
+    'getBlacklist' : IDL.Func([], [IDL.Vec(IDL.Principal)], []),
+    'getBlacklistQuery' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'getQueue' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Tuple(IDL.Principal, IDL.Text))],
+        [],
+      ),
+    'getQueueQuery' : IDL.Func(
+        [],
+        [IDL.Vec(IDL.Tuple(IDL.Principal, IDL.Text))],
+        ['query'],
+      ),
+    'getWhitelist' : IDL.Func([], [IDL.Vec(IDL.Principal)], []),
+    'getWhitelistQuery' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'isBlacklisted' : IDL.Func([IDL.Principal], [IDL.Bool], []),
+    'isBlacklistedQuery' : IDL.Func([IDL.Principal], [IDL.Bool], ['query']),
+    'isQueued' : IDL.Func([IDL.Principal], [IDL.Bool], []),
+    'isQueuedQuery' : IDL.Func([IDL.Principal], [IDL.Bool], ['query']),
+    'isWhitelisted' : IDL.Func([IDL.Principal], [IDL.Bool], []),
+    'isWhitelistedQuery' : IDL.Func([IDL.Principal], [IDL.Bool], ['query']),
+    'registerCallback' : IDL.Func([], [], ['oneway']),
+  });
   return Whitelist;
 };
 export const init = ({ IDL }) => { return []; };
